548155904c27b0b2fdcb93e805b872f279185f9b
Lang Kenney <Lang411@gmail.com>
2016-02-17 23:55:22 -0800

    Updates to jQuery, completed OpenWeather API call and JSON object import

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 //Code to get users location. From FreeCodeCamp.org example 
 if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(function(position) {
-$("#locationData").html("Latitude: " + position.coords.latitude + "&deg;   +     Longitude: " + position.coords.longitude+"&deg;");
-});
-}
-myLat = position.coords.latitude;
-myLon = position.coords.longitude;
-/*
-//Get JSON from API		
-$("#getMessage").on("ready", function(){
-      $.getJSON("/json/cats.json", function(json) {
-$(".message").html(JSON.stringify(json));
-});
+	navigator.geolocation.getCurrentPosition(function(position) {
+		$("#locationData").html("Latitude: " + position.coords.latitude + "&deg;   +     Longitude: " + position.coords.longitude+"&deg;");
 
+
+		myLat = position.coords.latitude;
+		myLon = position.coords.longitude;
+		var WeatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat="+myLat+"&lon="+myLon+"&appid=b4e4ecd77fc06e6944ca938ab6e909be";
+//Get JSON from API	
+	//$(document).ready(function() {	
+		//$("#getMessage").on("click", function(){
+		//var WeatherAPI = "http://api.openweathermap.org/data/2.5/forecast/city?id=3837675&APPID=b4e4ecd77fc06e6944ca938ab6e909be";
+		console.log(WeatherAPI);
+		$.getJSON( WeatherAPI, function(json) {
+			console.log(json)
+			$("#WeatherData").html(JSON.stringify(json));
+			$("#yourCity").html('Your City: '+json.name);
+			$("#yourWeather").html('Your Weather: '+json.weather[0].description);
+			if (json.weather[0].main=='Rain'){
+				console.log('Rain')
+				var imageUrl = 'https://github.com/Lang411/LocalWeather/blob/master/Assets/04001_thepierofhanaleibay_1600x900.jpg?raw=true';
+				 $("body").css('background-image', 'url("https://github.com/Lang411/LocalWeather/blob/master/Assets/04001_thepierofhanaleibay_1600x900.jpg?raw=true")');
+			};
+			json.weather[0].description
+			$("#yourTemp").html('Your Temprature: '+(9/5*(json.main.temp - 273) + 32).toFixed(2)+'&deg;F');
+			$("#yourHumidity").html('Your Humidity: '+json.main.humidity+'%');
+		});
+		//});
+	});
+//});
+
+
+}
+else {
+	$("#locationData").html("Cant obtain your location. Check your browser setting.");
+}
+
+/*
 //get images from JSON returned object
 json.forEach(function(val) {
 var keys = Object.keys(val);
